389d4e818e679d57711b51c148e5e66df2e158b9
Hascihan26 <hasancihanbilgin@gmail.com>
2024-08-28 15:32:48 +0300

    Initial Commit

---
+++ SwiftUIComponentsnew/SwiftUIComponentsnewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsnewApp.swift
+//  SwiftUIComponentsnew
+//
+//  Created by Hasan Cihan Bilgin on 28.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsnewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
